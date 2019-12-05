cada26e6663c91d60fb9f87c43e88a94dc8f0052
claudiogoncalves <claudiogoncalveslck@gmail.com>
2019-12-05 02:07:53 +0000

    commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-  $("#btn").click(function() {
-    var request = $.ajax({
-      url:
-        "https://api.unsplash.com/photos?order_by=latest&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
+  var bootstrapCol = "teste";
+  var request = $.ajax({
+    url:
+      "https://api.unsplash.com/photos?order_by=latest&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
 
-      success: function(data) {
-        console.log("success", data);
+    success: function(data) {
+      console.log("success", data);
+      for (let i = 0; i < data.length; i++) {
+        console.log(i);
       }
-    });
+      $("#wrapper>.row").append($("<div>", { class: bootstrapCol }));
+    }
   });
 });
