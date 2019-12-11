b7179f50485546a464ad73ba5abec43d058b4834
claudiogoncalves <claudiogoncalveslck@gmail.com>
2019-12-11 03:03:04 +0000

    commit

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-$(document).ready(function() {
+/*
+$(document).ready(function () {
   var bootstrapCol = "teste";
   var request = $.ajax({
-    url:
-      "https://api.unsplash.com/photos?order_by=latest&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
+    url: "https://api.unsplash.com/photos?order_by=latest&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
 
-    success: function(data) {
+    success: function (data) {
       console.log("success", data);
       for (let i = 0; i < data.length; i++) {
         console.log(i);
       }
-      $("#wrapper>.row").append($("<div>", { class: bootstrapCol }));
+      $("#wrapper>.row").append($("<div>", {
+        class: bootstrapCol
+      }));
     }
   });
 });
+*/
+/*
+$(document).ready(function () {
+  var bootstrapCol = "teste";
+  var request = $.ajax({
+    url: "https://api.unsplash.com/photos?random&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
+    type: "get",
+    async: true,
+    success: function (data, status, responseTxt) {
+      console.log(responseTxt.responseJSON);
+      $('.imagem').attr("src", responseTxt.responseJSON[1].urls.thumb)
+
+      $("#wrapper>.row").append($("<div>", {
+        class: bootstrapCol
+      }));
+    }
+  });
+});
+*/
