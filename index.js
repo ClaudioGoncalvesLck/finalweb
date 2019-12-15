c9d04949ab56ea2e357791b24afb7112b19429dc
claudiogoncalves <claudiogoncalveslck@gmail.com>
2019-12-15 01:16:01 +0000

    update imagens/profile/desc on load

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 $(document).ready(function () {
+  var request = $.ajax({
+    url: "https://api.unsplash.com/photos?order_by=latest&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
+
+    success: function (data) {
+      var img = document.querySelectorAll(".card-img");
+      var title = document.querySelectorAll(".card-title");
+      var profile = document.querySelectorAll(".profile img");
+      /*
+      var download = document.querySelectorAll(".transfer a ");
+      */
+      console.log(data);
+      for (i = 0; i < img.length; i++) {
+        $(img[i]).attr("src", data[i].urls.regular);
+        $(title[i]).html(data[i].description);
+        $(profile[i]).attr("src", data[i].user.profile_image.small);
+        /*
+        $(download[i]).attr("href", data[i].links.download_location);
+        */
+      }
+    }
+  });
+});
+
+/*
+$(document).ready(function () {
+  var request = $.ajax({
+    url: "https://api.unsplash.com/photos?order_by=latest&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
+
+    success: function (data) {
+      console.log("success", data);
+      for (i = 0; i < data.length; i++) {
+        $('.card-img').attr("src", data[i].urls.thumb)
+      }
+    }
+  });
+});
+*/
+
+/*
+$(document).ready(function () {
   var bootstrapCol = "teste";
   var request = $.ajax({
     url: "https://api.unsplash.com/photos?order_by=latest&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
@@ -8,24 +48,7 @@ $(document).ready(function () {
       for (let i = 0; i < data.length; i++) {
         console.log(i);
       }
-    }
-  });
-});
-
-/*
-$(document).ready(function () {
-  var bootstrapCol = "teste";
-  var request = $.ajax({
-    url: "https://api.unsplash.com/photos?random&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578",
-    type: "get",
-    async: true,
-    success: function (data, status, responseTxt) {
-      console.log(responseTxt.responseJSON);
-      $('.imagem').attr("src", responseTxt.responseJSON[1].urls.thumb)
-
-      $("#wrapper>.row").append($("<div>", {
-        class: bootstrapCol
-      }));
+      $('.card-img').attr("src", data.responseTxt.responseJSON[1].urls.thumb)
     }
   });
 });
