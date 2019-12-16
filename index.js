80403e6617f50ab71754d234263d1ae0193fb822
claudiogoncalves <claudiogoncalveslck@gmail.com>
2019-12-16 20:32:25 +0000

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getPhotos() {
     url: url,
     type: "get",
     async: true,
-    success: function(data, status, response) {
+    success: function (data, status, response) {
       getphotosResult(data);
     }
   });
@@ -31,55 +31,28 @@ function createphotoCard(data) {
 
   var content = $(
     '      <div class="col-lg-3 col-md-4 col-sm-6"><div class="box card bg-dark text-white"><img src="' +
-      data.img +
-      '" class="card-img" alt="' +
-      data.imgAlt +
-      '" /><div class="card-img-overlay"><h5 class="card-title">' +
-      data.description +
-      '</h5><div class="card-bottom"><div class="profile"><img src="' +
-      data.userImg +
-      '" class="profile-img" alt="Autor" width="32" height="32" /><p class="card-text">' +
-      data.userName +
-      '</p></div><div class="transfer"><a href="' +
-      data.img +
-      '"><i class="fas fa-download"></i></a></div></div></div></div></div>'
+    data.img +
+    '" class="card-img" alt="' +
+    data.imgAlt +
+    '" /><div class="card-img-overlay"><h5 class="card-title">' +
+    data.description +
+    '</h5><div class="card-bottom"><div class="profile"><img src="' +
+    data.userImg +
+    '" class="profile-img" alt="Autor" width="32" height="32" /><p class="card-text">' +
+    data.userName +
+    '</p></div><div class="transfer"><a href="' +
+    data.img +
+    '"><i class="fas fa-download"></i></a></div></div></div></div></div>'
   );
 
   father.append(content);
 }
 
 function nextPage() {
-  $("#movies-content")
-    .children("div")
-    .remove();
 
-  var page = 0;
-  console.log(window.location.href.indexOf);
-
-  if (window.location.href.indexOf("page") != -1) {
-    console.log("primeiro");
-    page++;
-  } else {
-    page = 2;
-    console.log("segundo");
-  }
-
-  var url =
-    "https://api.unsplash.com/photos?page=" +
-    page +
-    "&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578";
-
-  $.ajax({
-    url: url,
-    type: "get",
-    async: true,
-    success: function(data, status, response) {
-      getMoviesResult(data);
-    }
-  });
 }
 
-$("form").submit(function(event) {
+$("form").submit(function (event) {
   event.preventDefault();
 
   if ($("input").val()) {
@@ -92,7 +65,7 @@ $("form").submit(function(event) {
 });
 
 function getPhotosBySearch(search) {
-  var url2 =
+  var url =
     "https://api.unsplash.com/photos?page=1&query=" +
     search +
     "&client_id=dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578";
@@ -102,10 +75,10 @@ function getPhotosBySearch(search) {
     .remove();
 
   $.ajax({
-    url: url2,
+    url: url,
     type: "get",
     async: true,
-    success: function(data, status, response) {
+    success: function (data, status, response) {
       getphotosResult(data);
     }
   });
